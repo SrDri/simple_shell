1146c1e4136f80e65fb24d83d6528bb39620a351
Carlos Galeano <dnny419@gmail.com>
2021-04-08 05:24:18 +0700

    First commit

---
+++ _strcat.c
@@ -0,0 +1,16 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: what to add to the destination string
+ * Return: void
+ */
+void _strcat(char *dest, const char *src)
+{
+	int i;
+	int len = _strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[len++] = src[i];
+
+	dest[len] = '\0';
+}
