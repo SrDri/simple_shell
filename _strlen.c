a2dfdc28836715a02700af7c55badd63d103b078
Carlos Galeano <dnny419@gmail.com>
2021-04-08 05:27:02 +0700

    Strlen function

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * _strlen - finds the length of the string
+ * @s: a string constant
+ *
+ * Return: the length of the string s
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
