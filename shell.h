06ffe59956590756710d6296db9981551044d6f2
SrDri <juanjcarabali@gmail.com>
2021-04-12 22:01:19 +0700

    Betty libraries

--- shell.h
+++ shell.h
@@ -12,16 +12,20 @@
 #include <sys/stat.h>
 
 int _putchar(char);
-char **_strtok(char *s);
 void _prompt(int ac);
+
+char **_strtok(char *s);
 void _strcat(char *dest, const char *src);
 bool _strcmp(const char *s1, const char *s2, int beg, int end);
 void _strcpy(char *dest, const char *src);
 int _strlen(const char *s);
+
 void exit_control(char *line, ssize_t length);
+
 char *find_path(char **env, char *cmd);
 void get_strseq(const char *s, int beg, int end, char **res);
 int find_strseq(const char *s, const char *sep, int idx);
+
 bool env_built(char *s, char *env[]);
 void print_env(char **env);
 
